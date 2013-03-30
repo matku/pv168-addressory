3864330fa6bccdaee93d8b359cdbb56cc76e5229
matku555 <matku555@gmail.com>
2013-03-30 22:30:32 +0000

    with DataSource instead of DriverManager

--- pv168-addressory/src/main/java/cz/muni/fi/pv168/projekt/pv168_semestralny_projekt/Addressory.java
+++ pv168-addressory/src/main/java/cz/muni/fi/pv168/projekt/pv168_semestralny_projekt/Addressory.java
@@ -7,6 +7,7 @@ package cz.muni.fi.pv168.projekt.pv168_semestralny_projekt;
 import java.util.Map;
 import java.util.TreeMap;
 import javax.sql.DataSource;
+import org.apache.commons.dbcp.BasicDataSource;
 
 /**
  *
@@ -14,11 +15,21 @@ import javax.sql.DataSource;
  */
 public class Addressory 
 {
+    private static DataSource ds;
+    
     public static void main(String[] args) 
     {
-        ContactManagerImpl mngr = new ContactManagerImpl();
-        Contact cont = mngr.findContactByID(110l);
-        if (cont != null) System.out.println(cont.toString());
+        BasicDataSource bds = new BasicDataSource();
+        bds.setUrl("jdbc:derby://localhost:1527/skuska");
+        bds.setUsername("martin");
+        bds.setPassword("password");
+        ds = bds;
+        ContactManagerImpl mngr = new ContactManagerImpl(ds);
+        /*Contact cont = mngr.findContactByID(110l);
+        if (cont != null) 
+        {
+            System.out.println(cont.toString());
+        }*/
         
     }
     
