4caa4c31726907ea26d4f3713ce8595caa255a85
matku555@gmail.com <matku555@gmail.com@a28e4473-dcde-558d-c140-1cee0d091180>
2013-03-30 21:56:31 +0000

    create all tables

---
+++ trunk/pv168-addressory/src/main/java/cz/muni/fi/pv168/projekt/pv168_semestralny_projekt/createTables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE contact (
+id INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+first_name VARCHAR(20),
+last_name VARCHAR(20),
+address VARCHAR(50));
+
+CREATE TABLE numbers
+(contact_id INTEGER,
+type VARCHAR(10),
+number VARCHAR(15),
+FOREIGN KEY (contact_id) REFERENCES contact(id));
+
+CREATE TABLE groups
+(id INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+type VARCHAR(10),
+note VARCHAR(255));
+
+CREATE TABLE entry
+(contact_id INTEGER,
+group_id INTEGER,
+PRIMARY KEY (contact_id, group_id),
+FOREIGN KEY (contact_id) REFERENCES contact(id),
+FOREIGN KEY (group_id) REFERENCES contact(id));
