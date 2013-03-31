a7e435b4e6a53fd6689bfbd80160299432109c75
matku555 <matku555@gmail.com>
2013-03-31 10:44:26 +0000

    with setAutocommit...

--- pv168-addressory/src/main/java/cz/muni/fi/pv168/projekt/pv168_semestralny_projekt/ContactManagerImpl.java
+++ pv168-addressory/src/main/java/cz/muni/fi/pv168/projekt/pv168_semestralny_projekt/ContactManagerImpl.java
@@ -44,136 +44,210 @@ public class ContactManagerImpl implements ContactManager
     @Override
     public void newContact(Contact contact) 
     {
-        if (contact == null)
-        {
-            throw new IllegalArgumentException("contact must not be null");
-        }
+        validateContact(contact);
         if (contact.getId() != null)
         {
             throw new IllegalArgumentException("contact is already in the database");
         }
-        validateContact(contact);
-                
-        try (
-                Connection conn = ds.getConnection();//DriverManager.getConnection(URL, USER, PASSWORD);
-                PreparedStatement st1 = conn.prepareStatement(
-                    "INSERT INTO contact (first_name, last_name, address) VALUES (?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
-                PreparedStatement st2 = conn.prepareStatement(
-                    "INSERT INTO numbers (contact_id, type, number) VALUES (?, ?, ?)");
-                
-                )
+        
+        Connection conn = null;
+        try
         {
-            st1.setString(1, contact.getName());
-            st1.setString(2, contact.getSurname());
-            st1.setString(3, contact.getAddress());
-            int count = st1.executeUpdate();
-            assert count == 1;              //other than 1 line was created
-            
-            ResultSet keys = st1.getGeneratedKeys();
-            Long id = null;
-            if (keys.next())
+            conn = ds.getConnection();
+        
+            try (
+                    PreparedStatement st1 = conn.prepareStatement(
+                        "INSERT INTO contact (first_name, last_name, address) VALUES (?, ?, ?)",
+                        Statement.RETURN_GENERATED_KEYS);
+                    PreparedStatement st2 = conn.prepareStatement(
+                        "INSERT INTO numbers (contact_id, type, number) VALUES (?, ?, ?)");
+
+                    )
             {
-                id = keys.getLong(1);
-                contact.setId(id);
-            }
-            else
+                conn.setAutoCommit(false);
+                st1.setString(1, contact.getName());
+                st1.setString(2, contact.getSurname());
+                st1.setString(3, contact.getAddress());
+                int count = st1.executeUpdate();
+                assert count == 1;              //other than 1 line was created
+
+                ResultSet keys = st1.getGeneratedKeys();
+                Long id = null;
+                if (keys.next())
+                {
+                    id = keys.getLong(1);
+                    contact.setId(id);
+                }
+                else
+                {
+                    throw new RuntimeException("did not add to the database properly"
+                                            + "or did not return ID");
+                }
+
+                st2.setLong(1, id);
+                for (Map.Entry<String, NumberType> entry : contact.getPhoneNumbers().entrySet())
+                {
+                    st2.setString(3, entry.getKey());
+                    st2.setString(2, entry.getValue().name());
+                    int countt = st2.executeUpdate();
+                    assert countt == 1;          //other than 1 line was added
+                }
+                conn.commit();
+                conn.setAutoCommit(true);
+            } catch (SQLException e)
             {
-                throw new RuntimeException("did not add to the database properly"
-                                        + "or did not return ID");
-            }
-            
-            st2.setLong(1, id);
-            for (Map.Entry<String, NumberType> entry : contact.getPhoneNumbers().entrySet())
-            {
-                st2.setString(3, entry.getKey());
-                st2.setString(2, entry.getValue().name());
-                int countt = st2.executeUpdate();
-                assert countt == 1;          //other than 1 line was added
+                conn.rollback();
+                conn.setAutoCommit(true);
+                LOGGER.log(Level.SEVERE, "Error when inserting contact into DB", e);
+                throw new RuntimeException("Error when inserting into DB", e);
             }
         } catch (SQLException e)
         {
-            LOGGER.log(Level.SEVERE, "Error when inserting contact into DB", e);
-            throw new RuntimeException("Error when inserting into DB", e);
+            String msg = "Error when setting connection";
+            LOGGER.log(Level.SEVERE, msg, e);
+            throw new RuntimeException(msg, e);
+        } finally
+        {
+            if (conn != null)
+            {
+                try
+                {
+                    conn.close();
+                } catch (SQLException ex)
+                {
+                    LOGGER.log(Level.SEVERE, "Error when closing connection", ex);
+                    throw new RuntimeException("Error when closing connection", ex);
+                }
+            }
         }
     }
 
     @Override
     public void editContact(Contact contact) 
     {
-        if (contact == null)
-        {
-            throw new IllegalArgumentException("contact must not be null");
-        }
+        validateContact(contact);
         if (contact.getId() == null)
         {
             throw new IllegalArgumentException("contact is not in the database");
         }
-        validateContact(contact);
         
-        try (
-                Connection conn = ds.getConnection();//DriverManager.getConnection(URL, USER, PASSWORD);
-                PreparedStatement st1 = conn.prepareStatement(
-                    "UPDATE contact SET first_name = ?, last_name = ?, address = ? WHERE id = ?");
-                PreparedStatement st2 = conn.prepareStatement(
-                    "INSERT INTO numbers (contact_id, type, number) VALUES (?, ?, ?)");
-                PreparedStatement st3 = conn.prepareStatement(
-                    "DELETE FROM numbers WHERE contact_id = ?");
-                )
+        Connection conn = null;
+        
+        try
         {
-            st1.setString(1, contact.getName());
-            st1.setString(2, contact.getSurname());
-            st1.setString(3, contact.getAddress());
-            st1.setLong(4, contact.getId());
-            int count = st1.executeUpdate();
-            assert count == 1;              //other than 1 line was updated
-            
-            st3.setLong(1, contact.getId());
-            st3.executeUpdate();
-            st2.setLong(1, contact.getId());
-            for (Map.Entry<String, NumberType> entry : contact.getPhoneNumbers().entrySet())
+            conn = ds.getConnection();
+            try (
+                    PreparedStatement st1 = conn.prepareStatement(
+                        "UPDATE contact SET first_name = ?, last_name = ?, address = ? WHERE id = ?");
+                    PreparedStatement st2 = conn.prepareStatement(
+                        "INSERT INTO numbers (contact_id, type, number) VALUES (?, ?, ?)");
+                    PreparedStatement st3 = conn.prepareStatement(
+                        "DELETE FROM numbers WHERE contact_id = ?");
+                    )
             {
-                st2.setString(3, entry.getKey());
-                st2.setString(2, entry.getValue().name());
-                int countt = st2.executeUpdate();
-                assert countt == 1;          //other than 1 line was updated
+                conn.setAutoCommit(false);
+                st1.setString(1, contact.getName());
+                st1.setString(2, contact.getSurname());
+                st1.setString(3, contact.getAddress());
+                st1.setLong(4, contact.getId());
+                int count = st1.executeUpdate();
+                assert count == 1;              //other than 1 line was updated
+
+                st3.setLong(1, contact.getId());
+                st3.executeUpdate();
+                st2.setLong(1, contact.getId());
+                for (Map.Entry<String, NumberType> entry : contact.getPhoneNumbers().entrySet())
+                {
+                    st2.setString(3, entry.getKey());
+                    st2.setString(2, entry.getValue().name());
+                    int countt = st2.executeUpdate();
+                    assert countt == 1;          //other than 1 line was updated
+                }
+                conn.commit();
+                conn.setAutoCommit(true);
+            } catch (SQLException e)
+            {
+                conn.rollback();
+                conn.setAutoCommit(true);
+                LOGGER.log(Level.SEVERE, "Error when inserting contact into DB", e);
+                throw new RuntimeException("Error when inserting into DB", e);
             }
         } catch (SQLException e)
         {
-            LOGGER.log(Level.SEVERE, "Error when inserting contact into DB", e);
-            throw new RuntimeException("Error when inserting into DB", e);
+            String msg = "Error when setting connection";
+            LOGGER.log(Level.SEVERE, msg, e);
+            throw new RuntimeException(msg, e);
+        } finally
+        {
+            if (conn != null)
+            {
+                try
+                {
+                    conn.close();
+                } catch (SQLException ex)
+                {
+                    LOGGER.log(Level.SEVERE, "Error when closing connection", ex);
+                    throw new RuntimeException("Error when closing connection", ex);
+                }
+            }
         }
     }
 
     @Override
     public void deleteContact(Contact contact) 
     {
-        if (contact == null)
-        {
-            throw new IllegalArgumentException("contact must not be null");
-        }
+        validateContact(contact);
         if (contact.getId() == null)
         {
             throw new IllegalArgumentException("contact not in database");
         }
-                
-        try (
-                Connection conn = ds.getConnection();//DriverManager.getConnection(URL, USER, PASSWORD);
-                PreparedStatement st1 = conn.prepareStatement(
-                    "DELETE FROM numbers WHERE contact_id = ?");            
-                PreparedStatement st2 = conn.prepareStatement(
-                    "DELETE FROM contact WHERE id = ?");
-                )
+        
+        Connection conn = null;
+        
+        try
         {
-            st1.setLong(1, contact.getId());
-            st2.setLong(1, contact.getId());
-            st1.executeUpdate();
-            int count = st2.executeUpdate();
-            assert count <= 1;                  //should delete only one contact
+            conn = ds.getConnection();  
+            
+            try (
+                    PreparedStatement st1 = conn.prepareStatement(
+                        "DELETE FROM numbers WHERE contact_id = ?");            
+                    PreparedStatement st2 = conn.prepareStatement(
+                        "DELETE FROM contact WHERE id = ?");
+                    )
+            {
+                conn.setAutoCommit(false);
+                st1.setLong(1, contact.getId());
+                st2.setLong(1, contact.getId());
+                st1.executeUpdate();
+                int count = st2.executeUpdate();
+                assert count <= 1;                  //should delete only one contact
+                conn.commit();
+                conn.setAutoCommit(true);
+            } catch (SQLException e)
+            {
+                conn.rollback();
+                conn.setAutoCommit(true);
+                String msg = "Removing contact / numbers failed.";
+                LOGGER.log(Level.SEVERE, msg, e);
+            }
         } catch (SQLException e)
         {
-            String msg = "Removing contact / numbers failed.";
+            String msg = "Error when setting connection";
             LOGGER.log(Level.SEVERE, msg, e);
+            throw new RuntimeException(msg, e);
+        } finally
+        {
+            if (conn != null)
+            {
+                try
+                {
+                    conn.close();
+                } catch (SQLException ex)
+                {
+                    LOGGER.log(Level.SEVERE, "Error when closing connection", ex);
+                    throw new RuntimeException("Error when closing connection", ex);
+                }
+            }
         }
     }
 
@@ -195,6 +269,7 @@ public class ContactManagerImpl implements ContactManager
                         "SELECT * FROM numbers WHERE contact_id = ?");
                 )
         {
+            conn.setAutoCommit(false);
             st1.setLong(1, id);
             ResultSet rs = st1.executeQuery();
             if (rs.next())
@@ -217,6 +292,7 @@ public class ContactManagerImpl implements ContactManager
                 map.put(rs.getString("number"), Enum.valueOf(NumberType.class, rs.getString("type")));
             }
             contact.setPhoneNumbers(map);
+            conn.commit();
             return contact;
         } catch (SQLException e)
         {
@@ -244,6 +320,7 @@ public class ContactManagerImpl implements ContactManager
                         "SELECT * FROM numbers WHERE contact_id = ?");
                 )
         {
+            conn.setAutoCommit(false);
             st1.setString(1, names[0]);
             st1.setString(2, names[1]);
             ResultSet rs = st1.executeQuery();
@@ -267,6 +344,7 @@ public class ContactManagerImpl implements ContactManager
                 map.put(rs.getString("number"), Enum.valueOf(NumberType.class, rs.getString("type")));
             }
             contact.setPhoneNumbers(map);
+            conn.commit();
             return contact;
         } catch (SQLException e)
         {
@@ -287,6 +365,7 @@ public class ContactManagerImpl implements ContactManager
                         "SELECT * FROM numbers WHERE contact_id = ?");
                 )
         {
+            conn.setAutoCommit(false);
             ResultSet rs = st1.executeQuery();
             while(rs.next())
             {
@@ -310,17 +389,21 @@ public class ContactManagerImpl implements ContactManager
                 }
                 contact.setPhoneNumbers(map);
             }
+            conn.commit();
+            return list;
         } catch (SQLException e)
         {
             LOGGER.log(Level.SEVERE, "Error when selecting contact from DB", e);
             throw new RuntimeException("Error when selecting from DB", e);
         }
-        
-        return list;
     }
     
     private boolean validateContact(Contact contact)
     {
+        if (contact == null)
+        {
+            throw new IllegalArgumentException("contact is null");
+        }
         if (contact.getName() == null || contact.getName().equals(""))
         {
             throw new IllegalArgumentException("first name must not be null or empty");
